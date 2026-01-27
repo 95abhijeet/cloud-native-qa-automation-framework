0a9cc9620305c1e4d5ec6dd7601ce483875db67e
Abhijeet <95abhijeet@gmail.com>
2026-01-26 22:15:19 -0500

    Updates

--- src/test/java/com/abhijeet/qa/base/BaseTest.java
+++ src/test/java/com/abhijeet/qa/base/BaseTest.java
@@ -22,7 +22,7 @@ public class BaseTest {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(
                 new BrowserType.LaunchOptions()
-                        .setHeadless(false)
+                        .setHeadless(true)
                         .setSlowMo(50) //Slight delay helps stability in complex banking UIs
         );
         context = browser.newContext(new Browser.NewContextOptions()
